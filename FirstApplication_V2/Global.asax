adcd4bfceba9157ae06dc32fdec919a80ccfe683
Justin Schieck <schieck91@gmail.com>
2017-06-01 21:05:26 -0400

    Add project files.

---
+++ FirstApplication_V2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstApplication_V2.MvcApplication" Language="C#" %>
---
+++ FirstApplication_V2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstApplication_V2.Startup))]
+namespace FirstApplication_V2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
